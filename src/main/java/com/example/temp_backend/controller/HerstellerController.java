4c0763c62339105e3153ddc5eadb3b74f88e85a2
ibozorus <korkmazie@gmail.com>
2023-11-02 10:36:16 +0100

    - Initial Commit 3

--- src/main/java/com/example/temp_backend/controller/HerstellerController.java
+++ src/main/java/com/example/temp_backend/controller/HerstellerController.java
@@ -3,10 +3,9 @@ package com.example.temp_backend.controller;
 import com.example.temp_backend.model.Hersteller;
 import com.example.temp_backend.repository.HerstellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/hersteller")
@@ -18,6 +17,34 @@ public class HerstellerController {
     @GetMapping(path = "/all")
     public @ResponseBody Iterable<Hersteller> getAllHersteller() {
         return herstellerRepository.findAll();
+    }
+    @GetMapping(path = "/{id}")
+    public @ResponseBody Optional<Hersteller> getHerstellerById(@PathVariable Long id) {
+        return herstellerRepository.findById(id);
+    }
+
+    @PostMapping
+    Hersteller insertHersteller(@RequestBody Hersteller newHersteller) {
+        return herstellerRepository.save(newHersteller);
+    }
+
+    @PutMapping("/{id}")
+    Hersteller replaceHersteller(@RequestBody Hersteller newHersteller, @PathVariable Long id) {
+
+        return herstellerRepository.findById(id)
+                .map(Hersteller -> {
+                    Hersteller.setBrand(newHersteller.getBrand());
+                    return herstellerRepository.save(Hersteller);
+                })
+                .orElseGet(() -> {
+                    newHersteller.setId(id);
+                    return herstellerRepository.save(newHersteller);
+                });
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteHersteller(@PathVariable Long id) {
+        herstellerRepository.deleteById(id);
     }
 
 }
