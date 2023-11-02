fcba2603d435f1db1ff8b21cad0c578be0eb773a
ibozorus <korkmazie@gmail.com>
2023-11-02 12:10:13 +0100

    - SQL-Dateien hinzugefügt

---
+++ SQL/insert/hersteller.sql
@@ -0,0 +1,5 @@
+insert into hersteller (brand) values ('ACTIMMUNE');
+insert into hersteller (brand) values ('CHILDRENS ADVIL');
+insert into hersteller (brand) values ('Cialis');
+insert into hersteller (brand) values ('Lasix');
+insert into hersteller (brand) values ('Cerebraplex');
---
+++ SQL/insert/servercabin.sql
@@ -0,0 +1,10 @@
+insert into servercabin (floor) values (1);
+insert into servercabin (floor) values (5);
+insert into servercabin (floor) values (4);
+insert into servercabin (floor) values (4);
+insert into servercabin (floor) values (5);
+insert into servercabin (floor) values (4);
+insert into servercabin (floor) values (4);
+insert into servercabin (floor) values (4);
+insert into servercabin (floor) values (0);
+insert into servercabin (floor) values (2);
