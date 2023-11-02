f466c0a59f6c418983b0f23c0c5fe385e7e78af0
ibozorus <korkmazie@gmail.com>
2023-11-02 08:47:43 +0100

    Initial Commit

---
+++ src/main/java/com/example/temp_backend/TempBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.temp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/temp_backend/TempBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.temp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
